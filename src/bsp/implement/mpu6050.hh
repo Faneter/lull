805edd09fcbef92d5395de2546ac52c527c4dc83
Faneter <Faneter@outlook.com>
2026-04-04 11:36:53 +0800

    feat: 添加IMU Concept，便于后续的开发与抽象

--- src/bsp/implement/mpu6050.hh
+++ src/bsp/implement/mpu6050.hh
@@ -23,19 +23,25 @@ namespace bsp
         class MPU6050
         {
         public:
-            IMURawData raw_data;
-            IMUData data;
-            IMUData offset_data                  = {};
-            hal::i2c::I2CTransaction transaction = {
-                .type          = hal::i2c::TransactionType::ReadMem,
-                .dev_addr      = static_cast<uint16_t>(MPU6050Reg::ADDRESS),
-                .mem_addr      = static_cast<uint16_t>(MPU6050Reg::ACCEL_XOUT_H),
-                .mem_addr_size = 1,
-                .data_ptr      = buffer,
-                .size          = 14,
-                .context       = this,
-                .user_callback = transaction_callback,
-            };
+            hal::i2c::I2CTransaction transaction()
+            {
+                return _transaction;
+            }
+
+            IMUData data()
+            {
+                return _data;
+            }
+
+            IMURawData raw_data()
+            {
+                return _raw_data;
+            }
+
+            void set_offset(IMUData offset)
+            {
+                _offset_data = offset;
+            }
 
             hal::Status init()
             {
@@ -76,13 +82,13 @@ namespace bsp
 
             void update()
             {
-                data.accel.x     = static_cast<float>(raw_data.accel.x) / 16384.0f - offset_data.accel.x;
-                data.accel.y     = static_cast<float>(raw_data.accel.y) / 16384.0f - offset_data.accel.y;
-                data.accel.z     = static_cast<float>(raw_data.accel.z) / 16384.0f - offset_data.accel.z;
-                data.gyro.x      = static_cast<float>(raw_data.gyro.x) / 131.0f - offset_data.gyro.x;
-                data.gyro.y      = static_cast<float>(raw_data.gyro.y) / 131.0f - offset_data.gyro.y;
-                data.gyro.z      = static_cast<float>(raw_data.gyro.z) / 131.0f - offset_data.gyro.z;
-                data.temperature = (static_cast<float>(raw_data.temperature) / 340.0) + 36.53;
+                _data.accel.x     = static_cast<float>(_raw_data.accel.x) / 16384.0f - _offset_data.accel.x;
+                _data.accel.y     = static_cast<float>(_raw_data.accel.y) / 16384.0f - _offset_data.accel.y;
+                _data.accel.z     = static_cast<float>(_raw_data.accel.z) / 16384.0f - _offset_data.accel.z;
+                _data.gyro.x      = static_cast<float>(_raw_data.gyro.x) / 131.0f - _offset_data.gyro.x;
+                _data.gyro.y      = static_cast<float>(_raw_data.gyro.y) / 131.0f - _offset_data.gyro.y;
+                _data.gyro.z      = static_cast<float>(_raw_data.gyro.z) / 131.0f - _offset_data.gyro.z;
+                _data.temperature = (static_cast<float>(_raw_data.temperature) / 340.0) + 36.53;
             }
 
             bool has_new_data()
@@ -96,19 +102,31 @@ namespace bsp
 
         private:
             uint8_t buffer[14];
-
+            IMURawData _raw_data;
+            IMUData _data;
+            IMUData _offset_data                  = {};
+            hal::i2c::I2CTransaction _transaction = {
+                .type          = hal::i2c::TransactionType::ReadMem,
+                .dev_addr      = static_cast<uint16_t>(MPU6050Reg::ADDRESS),
+                .mem_addr      = static_cast<uint16_t>(MPU6050Reg::ACCEL_XOUT_H),
+                .mem_addr_size = 1,
+                .data_ptr      = buffer,
+                .size          = 14,
+                .context       = this,
+                .user_callback = transaction_callback,
+            };
             volatile bool _new_data_flag = false;
 
             static void transaction_callback(hal::i2c::I2CTransaction *transaction)
             {
-                MPU6050 *instance              = static_cast<MPU6050 *>(transaction->context);
-                instance->raw_data.accel.x     = (int16_t)(transaction->data_ptr[0] << 8 | transaction->data_ptr[1]);
-                instance->raw_data.accel.y     = (int16_t)(transaction->data_ptr[2] << 8 | transaction->data_ptr[3]);
-                instance->raw_data.accel.z     = (int16_t)(transaction->data_ptr[4] << 8 | transaction->data_ptr[5]);
-                instance->raw_data.temperature = (int16_t)(transaction->data_ptr[6] << 8 | transaction->data_ptr[7]);
-                instance->raw_data.gyro.x      = (int16_t)(transaction->data_ptr[8] << 8 | transaction->data_ptr[9]);
-                instance->raw_data.gyro.y      = (int16_t)(transaction->data_ptr[10] << 8 | transaction->data_ptr[11]);
-                instance->raw_data.gyro.z      = (int16_t)(transaction->data_ptr[12] << 8 | transaction->data_ptr[13]);
+                MPU6050 *instance               = static_cast<MPU6050 *>(transaction->context);
+                instance->_raw_data.accel.x     = (int16_t)(transaction->data_ptr[0] << 8 | transaction->data_ptr[1]);
+                instance->_raw_data.accel.y     = (int16_t)(transaction->data_ptr[2] << 8 | transaction->data_ptr[3]);
+                instance->_raw_data.accel.z     = (int16_t)(transaction->data_ptr[4] << 8 | transaction->data_ptr[5]);
+                instance->_raw_data.temperature = (int16_t)(transaction->data_ptr[6] << 8 | transaction->data_ptr[7]);
+                instance->_raw_data.gyro.x      = (int16_t)(transaction->data_ptr[8] << 8 | transaction->data_ptr[9]);
+                instance->_raw_data.gyro.y      = (int16_t)(transaction->data_ptr[10] << 8 | transaction->data_ptr[11]);
+                instance->_raw_data.gyro.z      = (int16_t)(transaction->data_ptr[12] << 8 | transaction->data_ptr[13]);
 
                 instance->_new_data_flag = true;
             }
--- src/bsp/interface/imu.hh
+++ src/bsp/interface/imu.hh
@@ -33,5 +33,25 @@ namespace bsp
             Gyro gyro;
             float temperature;
         };
+
+        template <typename T>
+        concept IMUConcept = requires(T imu) {
+            // 初始化函数，返回 hal::Status
+            { imu.init() } -> std::same_as<hal::Status>;
+
+            // 提供给 I2C Handler 使用的 Transaction 结构
+            { imu.transaction() } -> std::same_as<hal::i2c::I2CTransaction>;
+
+            // 数据获取接口
+            { imu.data() } -> std::same_as<IMUData>;
+            { imu.raw_data() } -> std::same_as<IMURawData>;
+
+            // 逻辑处理接口
+            { imu.update() } -> std::same_as<void>;
+            { imu.has_new_data() } -> std::same_as<bool>;
+
+            // 零偏配置
+            { imu.offset_data } -> std::same_as<IMUData &>;
+        };
     } // namespace imu
 } // namespace bsp
