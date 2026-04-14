e074556745f2a0d007bc21edecb810dee00fc43b
Faneter <Faneter@outlook.com>
2026-04-14 21:30:23 +0800

    perf: 一些内存和运算上的优化

--- src/bsp/implement/mpu6050.hh
+++ src/bsp/implement/mpu6050.hh
@@ -109,11 +109,10 @@ namespace bsp
 
         private:
             hal::i2c::AbstractHandler &i2cHandler;
-
+            IMUData _data;
+            IMUData _offset_data = {};
             uint8_t buffer[14];
             IMURawData _raw_data;
-            IMUData _data;
-            IMUData _offset_data                  = {};
             hal::i2c::I2CTransaction _transaction = {
                 .type          = hal::i2c::TransactionType::ReadMem,
                 .dev_addr      = ADDRESS,
@@ -128,25 +127,29 @@ namespace bsp
 
             void calculate_data()
             {
-                _data.accel.x     = static_cast<float>(_raw_data.accel.x) / 16384.0f - _offset_data.accel.x;
-                _data.accel.y     = static_cast<float>(_raw_data.accel.y) / 16384.0f - _offset_data.accel.y;
-                _data.accel.z     = static_cast<float>(_raw_data.accel.z) / 16384.0f - _offset_data.accel.z;
-                _data.gyro.x      = static_cast<float>(_raw_data.gyro.x) / 131.0f - _offset_data.gyro.x;
-                _data.gyro.y      = static_cast<float>(_raw_data.gyro.y) / 131.0f - _offset_data.gyro.y;
-                _data.gyro.z      = static_cast<float>(_raw_data.gyro.z) / 131.0f - _offset_data.gyro.z;
-                _data.temperature = (static_cast<float>(_raw_data.temperature) / 340.0) + 36.53;
+                static constexpr float inv_accel_scale       = 1.0f / 16384.0f;
+                static constexpr float inv_gyro_scale        = 1.0f / 131.0f;
+                static constexpr float inv_temperature_scale = 1.0f / 340.0f;
+
+                _data.accel.x     = static_cast<float>(_raw_data.accel.x) * inv_accel_scale - _offset_data.accel.x;
+                _data.accel.y     = static_cast<float>(_raw_data.accel.y) * inv_accel_scale - _offset_data.accel.y;
+                _data.accel.z     = static_cast<float>(_raw_data.accel.z) * inv_accel_scale - _offset_data.accel.z;
+                _data.gyro.x      = static_cast<float>(_raw_data.gyro.x) * inv_gyro_scale - _offset_data.gyro.x;
+                _data.gyro.y      = static_cast<float>(_raw_data.gyro.y) * inv_gyro_scale - _offset_data.gyro.y;
+                _data.gyro.z      = static_cast<float>(_raw_data.gyro.z) * inv_gyro_scale - _offset_data.gyro.z;
+                _data.temperature = (static_cast<float>(_raw_data.temperature) * inv_temperature_scale) + 36.53;
             }
 
             static void transaction_callback(hal::i2c::I2CTransaction *transaction)
             {
                 MPU6050 *instance               = static_cast<MPU6050 *>(transaction->context);
-                instance->_raw_data.accel.x     = (int16_t)(transaction->data_ptr[0] << 8 | transaction->data_ptr[1]);
-                instance->_raw_data.accel.y     = (int16_t)(transaction->data_ptr[2] << 8 | transaction->data_ptr[3]);
-                instance->_raw_data.accel.z     = (int16_t)(transaction->data_ptr[4] << 8 | transaction->data_ptr[5]);
-                instance->_raw_data.temperature = (int16_t)(transaction->data_ptr[6] << 8 | transaction->data_ptr[7]);
-                instance->_raw_data.gyro.x      = (int16_t)(transaction->data_ptr[8] << 8 | transaction->data_ptr[9]);
-                instance->_raw_data.gyro.y      = (int16_t)(transaction->data_ptr[10] << 8 | transaction->data_ptr[11]);
-                instance->_raw_data.gyro.z      = (int16_t)(transaction->data_ptr[12] << 8 | transaction->data_ptr[13]);
+                instance->_raw_data.accel.x     = static_cast<uint16_t>(transaction->data_ptr[0] << 8 | transaction->data_ptr[1]);
+                instance->_raw_data.accel.y     = static_cast<uint16_t>(transaction->data_ptr[2] << 8 | transaction->data_ptr[3]);
+                instance->_raw_data.accel.z     = static_cast<uint16_t>(transaction->data_ptr[4] << 8 | transaction->data_ptr[5]);
+                instance->_raw_data.temperature = static_cast<uint16_t>(transaction->data_ptr[6] << 8 | transaction->data_ptr[7]);
+                instance->_raw_data.gyro.x      = static_cast<uint16_t>(transaction->data_ptr[8] << 8 | transaction->data_ptr[9]);
+                instance->_raw_data.gyro.y      = static_cast<uint16_t>(transaction->data_ptr[10] << 8 | transaction->data_ptr[11]);
+                instance->_raw_data.gyro.z      = static_cast<uint16_t>(transaction->data_ptr[12] << 8 | transaction->data_ptr[13]);
 
                 instance->calculate_data();
 
